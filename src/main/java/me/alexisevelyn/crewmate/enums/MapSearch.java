2dd1e904cbc931e1ea7e0a6baa809e7debb32960
Alexis Evelyn <alexis.a.evelyn@gmail.com>
2020-10-29 22:41:00 -0400

    Oops

--- src/main/java/me/alexisevelyn/crewmate/enums/MapSearch.java
+++ src/main/java/me/alexisevelyn/crewmate/enums/MapSearch.java
@@ -7,15 +7,15 @@ import java.util.HashMap;
 public enum MapSearch {
 	SKELD((byte)   0b0), // 0
 	MIRA_HQ((byte) 0b1), // 1
-	POLUS((byte)   0b10), // 4
+	POLUS((byte)   0b10), // 2
 
 	// These are deprecated, because I will use the map's proper name on map release.
-	@Deprecated STICKMIN((byte)  0b100), // 8
-	@Deprecated MAP_FIVE((byte)  0b1000), // 16
-	@Deprecated MAP_SIX((byte)   0b10000), // 32
-	@Deprecated MAP_SEVEN((byte) 0b100000), // 64
-	@Deprecated MAP_EIGHT((byte) 0b1000000), // 128
-	@Deprecated MAP_NINE((byte)  0b10000000); // 256
+	@Deprecated STICKMIN((byte)  0b100), // 4
+	@Deprecated MAP_FIVE((byte)  0b1000), // 8
+	@Deprecated MAP_SIX((byte)   0b10000), // 16
+	@Deprecated MAP_SEVEN((byte) 0b100000), // 32
+	@Deprecated MAP_EIGHT((byte) 0b1000000), // 64
+	@Deprecated MAP_NINE((byte)  0b10000000); // 128
 
 	private final byte map;
 
